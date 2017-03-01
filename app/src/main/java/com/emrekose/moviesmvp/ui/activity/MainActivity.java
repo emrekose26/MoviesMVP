d681ac5a61e0f11c698d4f393f9a03b63ba0ceca
Emre Köse <e.kose26@gmail.com>
2017-03-01 15:05:56 +0300

    ViewpagerAdapter and MainActivity design created

--- app/src/main/java/com/emrekose/moviesmvp/ui/activity/MainActivity.java
+++ app/src/main/java/com/emrekose/moviesmvp/ui/activity/MainActivity.java
@@ -1,15 +1,40 @@
 package com.emrekose.moviesmvp.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.emrekose.moviesmvp.R;
+import com.emrekose.moviesmvp.ui.adapter.ViewPagerAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.mainToolbar) Toolbar toolbar;
+    @BindView(R.id.tabs) TabLayout tabLayout;
+    @BindView(R.id.viewPager) ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        //adapter.addFragment(new PopularMoviesFragment(), getString(R.string.popular));
+        //adapter.addFragment(new TopRatedMoviesFragment(), getString(R.string.top_rated));
+        viewPager.setAdapter(adapter);
     }
 }
