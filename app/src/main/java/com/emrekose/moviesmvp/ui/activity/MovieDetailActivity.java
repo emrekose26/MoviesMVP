d366a14c2b6f6ada7638ac65609a4001c96c22be
Emre Köse <e.kose26@gmail.com>
2017-03-02 14:51:32 +0300

    Detail Activity design

--- app/src/main/java/com/emrekose/moviesmvp/ui/activity/MovieDetailActivity.java
+++ app/src/main/java/com/emrekose/moviesmvp/ui/activity/MovieDetailActivity.java
@@ -2,26 +2,36 @@ package com.emrekose.moviesmvp.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.emrekose.moviesmvp.MoviesApp;
 import com.emrekose.moviesmvp.R;
 import com.emrekose.moviesmvp.di.detail.DaggerDetailComponent;
 import com.emrekose.moviesmvp.di.detail.DetailModule;
 import com.emrekose.moviesmvp.event.PopularDetailEvent;
 import com.emrekose.moviesmvp.event.TopRatedDetailEvent;
+import com.emrekose.moviesmvp.util.Constants;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
-    @Inject
-    EventBus eventBus;
+    @Inject EventBus eventBus;
+
+    @BindView(R.id.coverImage) ImageView coverImage;
+    @BindView(R.id.detail_title) TextView detailTitle;
+    @BindView(R.id.detail_backdrop) ImageView detailBackdrop;
+    @BindView(R.id.release_date) TextView releaseDate;
+    @BindView(R.id.detail_overview) TextView detailOverview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +39,44 @@ public class MovieDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_detail);
         ButterKnife.bind(this);
 
+        // for transparent status bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
         initInjector();
     }
 
     @Subscribe(sticky = true)
     public void onPopularMovieDetailEvent(PopularDetailEvent event) {
-        Toast.makeText(this, event.getResults().getTitle(), Toast.LENGTH_SHORT).show();
+        detailTitle.setText(event.getResults().getTitle());
+        releaseDate.setText(event.getResults().getRelease_date());
+        detailOverview.setText(event.getResults().getOverview());
+
+        Glide.with(this)
+                .load(Constants.IMAGE_BASE_URL + Constants.IMAGE_W342 + event.getResults().getPoster_path())
+                .fitCenter()
+                .placeholder(R.drawable.movieicon)
+                .into(detailBackdrop);
+
+        Glide.with(this)
+                .load(Constants.IMAGE_BASE_URL + Constants.IMAGE_W780 + event.getResults().getBackdrop_path())
+                .into(coverImage);
     }
 
     @Subscribe(sticky = true)
     public void onTopRatedMovieDetail(TopRatedDetailEvent event) {
-        Toast.makeText(this, event.getResults().getTitle(), Toast.LENGTH_SHORT).show();
+        detailTitle.setText(event.getResults().getTitle());
+        releaseDate.setText(event.getResults().getRelease_date());
+        detailOverview.setText(event.getResults().getOverview());
+
+        Glide.with(this)
+                .load(Constants.IMAGE_BASE_URL + Constants.IMAGE_W342 + event.getResults().getPoster_path())
+                .fitCenter()
+                .placeholder(R.drawable.movieicon)
+                .into(detailBackdrop);
+
+        Glide.with(this)
+                .load(Constants.IMAGE_BASE_URL + Constants.IMAGE_W780 + event.getResults().getBackdrop_path())
+                .into(coverImage);
     }
 
     @Override
